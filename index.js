ccdbb76d2df9df44f5f553fcf66b6784757360d6
Tasreen Mowreen <tmowreen97@gmail.com>
2022-04-21 14:45:10 -0400

    Done.

--- index.js
+++ index.js
@@ -39,13 +39,20 @@ function createTimeOutEvent(employeeRecObj, date){
 
 }
 
-function hoursWorkedOnDate(employeeRecObj, date){
-  const timeIn= employeeRecObj.timeInEvents[0].hour.toString()
-  const timeOut = employeeRecObj.timeOutEvents[0].hour.toString()
-  if (date == employeeRecObj.timeInEvents[0].date){
-    const diff = (timeOut-timeIn).toString()
-    return parseInt(diff.slice(0, diff.length-2))
+function hoursWorkedOnDate(employeeRecObj, dateInput){
+  var timeIn =''
+  var timeOut=''
+  for (let i=0; i<employeeRecObj.timeInEvents.length; i++)
+  {
+    if (dateInput== employeeRecObj.timeInEvents[i].date){
+      timeIn = employeeRecObj.timeInEvents[i].hour.toString()
+      if (dateInput== employeeRecObj.timeOutEvents[i].date){
+        timeOut=(employeeRecObj.timeOutEvents[i].hour.toString())
+      }
+    }
   }
+  const diff = (timeOut-timeIn).toString()
+  return parseInt(diff.slice(0, diff.length-2))
 }
 
 function wagesEarnedOnDate(employeeRecObj, date){
@@ -58,43 +65,16 @@ function allWagesFor (employeeRecObj){
   var sum=0
   for (let i=0; i<employeeRecObj.timeInEvents.length; i++){
     let date = employeeRecObj.timeInEvents[i].date
-    console.log(date)
     const wages = (wagesEarnedOnDate(employeeRecObj,date))
-    console.log(wages)
     sum+=wages
   }
-  console.log (sum)
   return sum
 }
 
-// function allWagesFor(employeeRecObj){
-//   let dates = []
-//   for (let i=0; i<employeeRecObj.timeInEvents.length;i++){
-//     dates.push(employeeRecObj.timeInEvents[i].date)
-//   }
-//   console.log(dates)
-//   var sum =0
-//   for (const date in dates){
-//     sum += (wagesEarnedOnDate(employeeRecObj,date))
-//     console.log (sum)
-//   }
-//   return sum
-// }
-
-
-const example = ['Tasreen', 'Mowreen', 'Ms.', 18]
-const date = "2022-04-20 1440"
-console.log (createTimeInEvent(createEmployeeRecord(example), date))
-
-
-
-
-//PseudoCode
-// first we convert array into an object
-// we want the values to coincide with the values in the array  // var sum =0
-  // for (let i=0; i++) {
-  //   console.log(wagesEarnedOnDate(employeeRecObj, (employeeRecObj.timeInEvents[i].date)));
-    // sum+= wages
-  
-  // console.log(sum)
-  // return sum
+function calculatePayroll(arr){
+  let payroll = [];
+  arr.forEach(employee => {
+      payroll.push(allWagesFor(employee)) 
+  });
+  return payroll.reduce((previousValue, currentValue) => previousValue + currentValue)
+}
